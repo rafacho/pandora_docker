3c96c1b92d789805fb82c178a41d59b85eb89c4a
Rafacho <rafacho.hugo@gmail.com>
2026-08-25 12:20:37 -0300

    fix: rear wheels were rotating in the wrong direction

--- src/pandora_control/pandora_control/wheel_control.py
+++ src/pandora_control/pandora_control/wheel_control.py
@@ -3,7 +3,7 @@
 
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import TwistStamped
 
 PI = 3.1415926535897
 
@@ -11,17 +11,19 @@ PI = 3.1415926535897
 class WheelControl(Node):
     def __init__(self):
         super().__init__('move_wheels')
-        self.wheel_command = self.create_publisher(Twist, '/velocity_controller/cmd_vel', 1)
+        self.wheel_command = self.create_publisher(TwistStamped, '/velocity_controller/cmd_vel', 1)
         self.create_timer(1.0 / 25.0, self.run)
 
     def run(self):
-        b = Twist()
-        b.linear.x = -0.5  # -0.01 * time_sec
-        b.linear.y = 0.0
-        b.linear.z = 0.0
-        b.angular.x = 0.0
-        b.angular.y = 0.0
-        b.angular.z = 0.0  # -0.1 * time_sec
+        b = TwistStamped()
+        b.header.stamp = self.get_clock().now().to_msg()
+        b.header.frame_id = 'base_link'
+        b.twist.linear.x = -0.5  # -0.01 * time_sec
+        b.twist.linear.y = 0.0
+        b.twist.linear.z = 0.0
+        b.twist.angular.x = 0.0
+        b.twist.angular.y = 0.0
+        b.twist.angular.z = 0.0  # -0.1 * time_sec
 
         self.wheel_command.publish(b)
         print(b)
